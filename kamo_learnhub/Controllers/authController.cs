52c1e3440dd49332dcd87fb1af41cf3a0b787bd7
JosephMarota <u23642425@tuks.co.za>
2026-01-06 20:42:16 +0200

    Initial commit - Kamo Learnhub API

---
+++ kamo_learnhub/Controllers/authController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kamo_learnhub.Controllers
+{
+    public class authController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
