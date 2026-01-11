de99712bcf44e1a0cca86e09c5ff4d03676913b6
JosephMarota <u23642425@tuks.co.za>
2026-01-11 21:24:08 +0200

    Program code

--- kamo_learnhub/Program.cs
+++ kamo_learnhub/Program.cs
@@ -35,6 +35,17 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -55,6 +66,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowAngular");
 
 app.UseHttpsRedirection();
 
